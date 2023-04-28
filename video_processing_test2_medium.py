f380bbd61c9042ef2269f2e75895f6547ab6237a
gugi200 <78741702+gugi200@users.noreply.github.com>
2023-04-28 11:28:30 +0100

    Update video_processing_test2_medium.py

--- video_processing_test2_medium.py
+++ video_processing_test2_medium.py
@@ -13,21 +13,12 @@ from periphery import PWM
 import wiringpi
 import servo
 from synDefQuickMed import *
-
-import sys            
-
-
 class ProcessImage():
 
-    # test the blue thereshold
-    # and the ROI delta and mask delta
-    
     def __init__(self):
-
+        '''Initialize dynamic variables for video processing'''
         
-        # intialize video processing prameters
-        
-        # parameters for lead line
+        # variables for lead line
         self.contour_cond = 0
         self.contour_ok = 0
         self.nearest_idx_maze = np.array([0, 0])
@@ -41,12 +32,10 @@ class ProcessImage():
         self.prevBall = [0, 0]
         self.flickIter = 0
 
-        
-        # initialize servos parameters
+        # initialize servos 
         self.servos = servo.servo()
         self.currentAngle1 = 90
         self.currentAngle2 = 90
-
 
         # vector interation offset
         self.vectIter = 0
@@ -54,7 +43,7 @@ class ProcessImage():
 
     
     def getCanny(self, image):
-        '''get black and white image displaying edges'''
+        '''get black and white image displaying contours'''
         logging.debug("[getCanny]   Start")
         image[MASK_WALL[1]:MASK_WALL[3], MASK_WALL[0]:MASK_WALL[2]] = np.zeros(
                 ((MASK_WALL[3]-MASK_WALL[1]), MASK_WALL[2]-MASK_WALL[0], 3)).astype(np.uint8)
@@ -89,21 +78,23 @@ class ProcessImage():
     @staticmethod
     @jit(nopython=True)   
     def jitROI(image, delta, x0=0, y0=0, x1=0, y1=0):
-            x0_ROI = x0 - delta
-            x1_ROI = x1 + delta
-            y0_ROI = y0 - delta
-            y1_ROI = y1 + delta
+        '''get Region Of Interest for a given image and dimensions. 
+        Returns ROI image and the coo of the start - compiled version'''
+        x0_ROI = x0 - delta
+        x1_ROI = x1 + delta
+        y0_ROI = y0 - delta
+        y1_ROI = y1 + delta
+        
+        if x0_ROI<0:
+            x0_ROI=0
+        if x1_ROI>image.shape[1]:
+            x1_ROI = image.shape[1] - 1
+        if y0_ROI<0:
+            y0_ROI=0
+        if y1_ROI>image.shape[0]:
+            y1_ROI = image.shape[0] - 1
             
-            if x0_ROI<0:
-                x0_ROI=0
-            if x1_ROI>image.shape[1]:
-                x1_ROI = image.shape[1] - 1
-            if y0_ROI<0:
-                y0_ROI=0
-            if y1_ROI>image.shape[0]:
-                y1_ROI = image.shape[0] - 1
-                
-            return image[y0_ROI:y1_ROI, x0_ROI:x1_ROI], x0_ROI, y0_ROI
+        return image[y0_ROI:y1_ROI, x0_ROI:x1_ROI], x0_ROI, y0_ROI
                 
 
     def getLeadLine(self, cannyImg, lineFrame):
@@ -114,9 +105,6 @@ class ProcessImage():
         contours, _ = findContours(thresh, RETR_EXTERNAL, CHAIN_APPROX_NONE)
         self.contours = max(contours, key=contourArea)
         self.showLeadLine(lineFrame)
-        
-        # self.contour_ok = int(input("0 for not ok, 1 for ok: "))
-        
         self.contour_ok = 1
 
         if self.contour_ok:
@@ -137,13 +125,13 @@ class ProcessImage():
         
             
     def showLeadLine(self, frame):
-        '''show the the area the longest contour encloses'''
+        '''show the the area the longest contour'''
         drawContours(frame, [self.contours], -1, (0, 0, 255), FILLED)
         imshow('Lead line - longest line' ,frame)
         
         
     def detectHoles(self, frame, cannyImg):
-        
+        '''detect the holes'''
         _, thresh = threshold(cannyImg, 128, 255, THRESH_BINARY)
         contours, _ = findContours(thresh, RETR_EXTERNAL, CHAIN_APPROX_NONE)
         if len(contours) !=0:
@@ -161,35 +149,30 @@ class ProcessImage():
                     holeCount += 1
             logging.info("[detectHoles]    Number of holes detected: %i", holeCount)
             imshow("holes", frame)
-            #print("hole count:",holeCount)
             return holeContour
         return np.ones([1, 1])
         
     def flick(self):
-         ''''''
+         '''apply a sudden board movement if ball is stationary'''
          if self.flickIter%FLICK_OFFSET==0:
              if (self.prevBall[0]-STUCK_PIXELS<self.ball_coo[0]<self.prevBall[0]+STUCK_PIXELS) and (self.prevBall[1]-STUCK_PIXELS<self.ball_coo[1]<self.prevBall[1]+STUCK_PIXELS):
                  self.prevBall[0] = self.ball_coo[0]
                  self.prevBall[1] = self.ball_coo[1]
                  try:
                      self.runServo.terminate()
-                
                  except AttributeError:
                      logging.warning("[processWindowImg]   Threading error") 
 
                  self.runServo = Process(
                     target=self.servos.moveBothServos, args=(
                     10, 70))        
-              
                  self.runServo.start()
                  logging.error("SCANDI FLICK")
                  self.flickIter += 1
                  return 1
-                 
              else:
                  self.prevBall[0] = self.ball_coo[0]
                  self.prevBall[1] = self.ball_coo[1]
-                 
          self.flickIter += 1
          return 0
      
@@ -197,7 +180,7 @@ class ProcessImage():
     @staticmethod
     @jit(nopython=True)    
     def dodgeHoles(holeContour, xball_cen, yball_cen, ball_coo):
-
+        '''computes an anti-hole vector - compiled'''
         x0_ROI = ball_coo[0] - ROI_HOLES
         x1_ROI = ball_coo[0]+ball_coo[2] + ROI_HOLES
         y0_ROI = ball_coo[1] - ROI_HOLES
@@ -217,25 +200,10 @@ class ProcessImage():
             nearest_idx = nonzero[0]
             ToHoleY  = yball_cen - nearest_idx[0] - y0_ROI
             ToHoleX  = xball_cen - nearest_idx[1] - x0_ROI
-
             return np.sign(ToHoleX)*25, np.sign(ToHoleY)*25
         else:
             return 0, 0     
         
-   
-    def threadBallDetect(self):
-        while True:
-            '''find a blue object of specific dimensions and area'''
-            logging.debug("[detectBall]    Start")
-            image = self.ballFrame
-    
-            self.ball_coo = ProcessImage.detectBall(image)
-            
-            if not self.ball_coo[3]:
-                continue
-            self.xball_cen = int(self.ball_coo[0] + (self.ball_coo[2]/2))
-            self.yball_cen = int(self.ball_coo[1] + (self.ball_coo[3]/2))
-
             
             
     @staticmethod
@@ -268,63 +236,13 @@ class ProcessImage():
 
         else:
             return (0, 0, 0, 0)        
-            
-
-    
-    def maskRoute(self):
-        '''mask the route an object has travelled'''
-        logging.debug("[maskRoute]    Start")
-        
-        x0 = self.xball_cen-(MASK_DELTA/self.accuracyMode)
-        y0 = self.yball_cen-(MASK_DELTA/self.accuracyMode)
-        x1 = self.xball_cen+(MASK_DELTA/self.accuracyMode)
-        y1 = self.yball_cen+(MASK_DELTA/self.accuracyMode)
-
-        if x0 <= 0:
-            x0 = 0
-        if y0 <= 0:
-            y0 = 0
-        if x1 >= self.contour_img.shape[1]:
-            x1 = self.contour_img.shape[1] - 1
-        if y1 >= self.contour_img.shape[0]:
-            y1 = self.contour_img.shape[0] - 1
-
-        self.contour_img[y0:y1, x0:x1] = 0
-        self.contour_img_mask[y0:y1, x0:x1] = [0, 0, 0]       
-        logging.debug("[maskRoute]    End")
-   
-   
-    def unmaskRoute(self):
-        '''unmask the route an object has travelled'''
-        logging.debug("[unmaskRoute]    Start")
-        x0 = self.xball_cen-MASK_DELTA
-        y0 = self.yball_cen-MASK_DELTA
-        x1 = self.xball_cen+MASK_DELTA
-        y1 = self.yball_cen+MASK_DELTA
-
-        if x0 <= 0:
-            x0 = 0
-        if y0 <= 0:
-            y0 = 0
-        if x1 >= self.contour_img.shape[1]:
-            x1 = self.contour_img.shape[1] - 1
-        if y1 >= self.contour_img.shape[0]:
-            y1 = self.contour_img.shape[0] - 1        
-
-        contourROI, contX0, contY0 = self.getROI(
-            self.ogContourImg, 0, x0, y0, x1, y1)
-        self.contour_img[y0:y1, x0:x1] = contourROI
-
-        self.contour_img_mask[y0:y1, x0:x1] = [1, 1, 1] 
-        logging.info("[unmaskRoute]    The past route has been unmasked")
-        logging.debug("[unmaskRoute]    End")
-    
+             
 
     @staticmethod
     @jit(nopython=True)
     def getVector(contour, ball_coo, vectIter, nearest_idx_maze):
-        '''Find the vector between the closest line position and the current position of the object'''
-        # ROI for the vectoring
+        '''Find the vector between the closest line position 
+        and the current position of the object - compiled'''
         x0_ROI = ball_coo[0] - ROI_VECT
         x1_ROI = ball_coo[0]+ball_coo[2] + ROI_VECT
         y0_ROI = ball_coo[1] - ROI_VECT
@@ -369,6 +287,7 @@ class ProcessImage():
 
         
     def getRollingWindow(self, frame):
+        '''find the nodes along the lead line'''
         logging.debug("[getRollingWindow]    Start")
         self.ball_coo = ProcessImage.detectBall(frame)
         if self.ball_coo[3] ==0:
@@ -420,6 +339,7 @@ class ProcessImage():
     @staticmethod
     @jit(nopython=True)
     def windowGuts(xball_cen, yball_cen, nodes):
+        '''finds nodes for the visibility window - compiled'''
         accuracyMode = 1
         distances = np.sqrt(
                 np.square(nodes[0] - yball_cen) \
@@ -438,61 +358,12 @@ class ProcessImage():
             visableNodes = nodes.T[nearestNodeIdx+WINDOW_OFFSET:nearestNodeIdx+VIS_WINDOW_SIZE].reshape(-1,2).T
         return visableNodes, accuracyMode
         
-    def theySeeMeRollin(self):
-        ''''''
-        logging.debug("[moveRollingWindow]    Initilised")
-        
-        while True:
-            if (not self.xball_cen) and (not self.yball_cen):
-                continue
-            maskContour = np.zeros((self.contour_img.shape[0], self.contour_img.shape[1]))
-            visableNodes, accuracyMode = ProcessImage.windowGuts(self.xball_cen, self.yball_cen, self.nodes)
-            maskContour[visableNodes[0], visableNodes[1]] = 1
-            self.maskContour = ndimage.binary_dilation(maskContour, iterations=NODE_WINDOW).astype(np.uint8)
-            logging.info("[moveRollingWindow]    Mask calculated")
-    
-        
-    def moveRollingWindow(self):
-        ''''''
-        logging.debug("[moveRollingWindow]    Initilised")
-        
-        while True:
-            if not self.xball_cen:
-                continue
-            
-            logging.debug("[moveRollingWindow]    Start")
-            maskContour = np.zeros((self.contour_img.shape[0], self.contour_img.shape[1]))
-            self.accuracyMode = 1
-            distances = np.sqrt(
-                    np.square(self.nodes[0] - self.yball_cen) \
-                        + np.square(self.nodes[1]-self.xball_cen))
 
-            nearestNodeIdx = np.argmin(distances)
-            nearest_idx = self.nodes.T[nearestNodeIdx]
-            logging.warning("The nearest node number: %i, last node: %i, total lenght: %i", 
-                            nearestNodeIdx, nearestNodeIdx+VIS_WINDOW_SIZE, len(self.nodes.T))
-            if (nearestNodeIdx+VIS_WINDOW_SIZE+2)>=len(self.nodes.T):
-                visableNodes = self.nodes.T[nearestNodeIdx+2:-1].reshape(-1,2).T
-            else:
-                abs_Y_dist = abs(self.nodes.T[nearestNodeIdx+VIS_WINDOW_SIZE][0]-nearest_idx[0])
-                abs_X_dist = abs(self.nodes.T[nearestNodeIdx+VIS_WINDOW_SIZE][1]-nearest_idx[1])
-                if (abs_Y_dist<HIGH_ACC_TH) and (abs_X_dist<HIGH_ACC_TH):
-                    logging.info("[moveRollingWindow]    Double accuracy mode")
-                    self.accuracyMode = 2
-                if abs_Y_dist<MEDIUM_ACC_TH and abs_X_dist<MEDIUM_ACC_TH:
-                    logging.info("[moveRollingWindow]    Accuracy mode")
-                    self.accuracyMode = 1.5
-                visableNodes = self.nodes.T[nearestNodeIdx+2:nearestNodeIdx+VIS_WINDOW_SIZE].reshape(-1,2).T
-                logging.warning("ACCURACY MODE: %f", self.accuracyMode) 
-
-            maskContour[visableNodes[0], visableNodes[1]] = 1
-            self.maskContour = ndimage.binary_dilation(maskContour, iterations=NODE_WINDOW).astype(np.uint8)
-            logging.info("[moveRollingWindow]    Mask calculated")
-            
         
     @staticmethod
     @jit(nopython=True)   
     def windowMasking(windowContour, windowFrame, xball_cen, yball_cen, accuracyMode):
+        '''masks the area around the ball - compiled'''
         x0 = xball_cen-int(MASK_DELTA/accuracyMode)
         y0 = yball_cen-int(MASK_DELTA/accuracyMode)
         x1 = xball_cen+int(MASK_DELTA/accuracyMode)
@@ -511,31 +382,6 @@ class ProcessImage():
         
         windowFrame[y0:y1, x0:x1] = np.array([0, 0, 0]).astype(np.uint8)       
         return windowContour, windowFrame
- 
-    @staticmethod
-    def moveWindowNoThread(contour_img, nodes, xball_cen, yball_cen):
-        ''''''
-        maskContour = np.zeros((contour_img.shape[0], contour_img.shape[1]))
-        accuracyMode = 1
-        distances = np.sqrt(
-                np.square(nodes[0] - yball_cen) \
-                    + np.square(nodes[1]-xball_cen))
-        nearestNodeIdx = np.argmin(distances)
-        nearest_idx = nodes.T[nearestNodeIdx]
-        if (nearestNodeIdx+VIS_WINDOW_SIZE+2)>=len(nodes.T):
-            visableNodes = nodes.T[nearestNodeIdx+1:-1].reshape(-1,2).T
-        else:
-            abs_Y_dist = abs(nodes.T[nearestNodeIdx+VIS_WINDOW_SIZE][0]-nearest_idx[0])
-            abs_X_dist = abs(nodes.T[nearestNodeIdx+VIS_WINDOW_SIZE][1]-nearest_idx[1])
-            if (abs_Y_dist<HIGH_ACC_TH) and (abs_X_dist<HIGH_ACC_TH):
-                accuracyMode = 30
-            if abs_Y_dist<MEDIUM_ACC_TH and abs_X_dist<MEDIUM_ACC_TH:
-                accuracyMode = 15
-            visableNodes = nodes.T[nearestNodeIdx+1:nearestNodeIdx+VIS_WINDOW_SIZE].reshape(-1,2).T
-        maskContour[visableNodes[0], visableNodes[1]] = 1
-        maskContour = ndimage.binary_dilation(maskContour, iterations=NODE_WINDOW).astype(np.uint8)
-        return maskContour
-            
 
 
     def showMaskedVector(self, frame, vector_coo, antiHoleVector):
@@ -581,6 +427,8 @@ class ProcessImage():
     def processWindowImg(self, frame):
         '''Run the processing software'''
         logging.debug("[processWindowImg]    Start")
+        
+        # prepare the frame
         frame, _, _ = ProcessImage.jitROI(frame, 0,
                                                 MAZE_ROI[0], MAZE_ROI[1], 
                                                 MAZE_ROI[2], MAZE_ROI[3]) 
@@ -588,17 +436,20 @@ class ProcessImage():
         self.ballFrame = np.copy(frame)
         cannyImg= self.getCanny(frame1)
     
-        
+        # find lead line
         if self.contour_ok==0:    
             lineFrame = np.copy(frame)
             if not self.getLeadLine(cannyImg, lineFrame):
                 return np.array([])
             self.holeContour = self.detectHoles(lineFrame, cannyImg)
 
+        # main loop content
         if self.contour_ok:  
             if self.restart:
                 return np.array([])
             # imshow("Original frame", frame)
+            
+            # find the ball
             self.ball_coo = (0, 0, 0, 0)
             count = 0
             while self.ball_coo[3]==0:
@@ -614,11 +465,15 @@ class ProcessImage():
                 logging.warning("[processWindowImg]    Ball not detected")
                 return np.array([])
             
+            # check if the maze is completed
             if abs(self.nodes[0][-1]-self.yball_cen)<COMPLETE_TH and abs(self.nodes[1][-1]-10-self.xball_cen)<COMPLETE_TH:
                 print("DONE DONE DONE DONE")
                 return np.array([0])
+            
+            # check for flick condition
             flickEnable = self.flick()
 
+            # create the window mask
             maskContour = np.zeros((self.contour_img.shape[0], self.contour_img.shape[1]))
             visableNodes, accuracyMode = ProcessImage.windowGuts(self.xball_cen, self.yball_cen, self.nodes)
             maskContour[visableNodes[0], visableNodes[1]] = 1
@@ -628,13 +483,12 @@ class ProcessImage():
             maskFrame = np.repeat(self.maskContour, 3).reshape(self.contour_img.shape[0], self.contour_img.shape[1], 3) * 255
             windowFrame = np.bitwise_and(frame, maskFrame.astype(np.uint8)) # get windowed frame
 
-            
             # get windowed lead line
-
             windowContour = np.copy(self.ogContourImg).astype(np.uint8)
 
             # imshow("mask", maskFrame)
 
+            # merge mask with the frame
             windowContour = np.bitwise_and(windowContour, self.maskContour)
 
             logging.warning("ACCURACY MODE: %f", accuracyMode)
@@ -643,12 +497,15 @@ class ProcessImage():
             Vx, Vy, self.nearest_idx_maze, self.vectIter, pid_reset = ProcessImage.getVector(
                 windowContour, self.ball_coo, self.vectIter, self.nearest_idx_maze)
 
+            # compute vectors
             if not Vx and not Vy:
                 return np.array([])
             antiHoleVectorX, antiHoleVectorY = ProcessImage.dodgeHoles(
                 self.holeContour, self.xball_cen, self.yball_cen, self.ball_coo)
             logging.warning("[processWindowImg]   The raw ball vector, x: %i, y: %i", Vx, Vy)
             logging.warning("[processWindowImg]   Inti-hole vector, x: %i, y: %i", antiHoleVectorX, antiHoleVectorY)
+            
+            # pass vector to the servo-controller
             finalAngle1, finalAngle2 =self.servos.PositionPDController(
                         Vx+antiHoleVectorX, Vy+antiHoleVectorY, pid_reset)
             
@@ -667,6 +524,7 @@ class ProcessImage():
               
                 self.runServo.start()
             
+            # display results
             logging.warning("[processWindowImg]    The net vector (%i, %i)", Vx+antiHoleVectorX, Vy+antiHoleVectorY)
             frameArrowed = self.showMaskedVector(windowFrame, [Vx, Vy], [antiHoleVectorX, antiHoleVectorY])
             # self.showBallVector(frame, [Vx, Vy], [antiHoleVectorX, antiHoleVectorY])
@@ -679,17 +537,16 @@ class ProcessImage():
 def live_feedV2():
     vid = VideoCapture(0)
     image = ProcessImage()
-    condition = True
     current_t = 0 
     timestamp_start = time.time()
-    # while(condition):
-    #     condition = int(input('give 0: '))
    
     while(True):
+        # read camera
         ret, frame = vid.read()
+        
+        # wait for camera to initialise
         if current_t>=3:
             complete = len(image.processWindowImg(frame))
-            # print(complete)
             if complete == 1:
                 break
         else:
@@ -701,42 +558,5 @@ def live_feedV2():
     destroyAllWindows()
 
 live_feedV2()
-
-def videoV2():
-    cap = VideoCapture('WIN_20230206_16_20_55_Pro.mp4')
-    # servos = servo.servo()
-    image = ProcessImage()
-
-    while(cap.isOpened()):
-        _, frame = cap.read()
-        img_75 = resize(frame, None, fx = 0.5, fy = 0.5)
-        imshow("OG frame", img_75)
-        # ROI_image_rgb, canny_image = get_ROI(img_75, 0, 0, 1000, 1000)
-        # line_image, lines, combo_image = get_lines(canny_image, img_75)
-        # ball_line_maze_image, lines_data, ball_data, canny_image,image_gray = process_image(img_75)
-        # cv2.imshow("combo_image after display_lines", ball_line_maze_image)
-        #time.sleep(0.1)
-        image.processWindowImg(img_75)
-        #time.sleep(0.1)
-        if waitKey(1) & 0xFF == ord('q'):
-            break
-# videoV2()
-
-#if __name__ == "__main__":
-    # maze = multiprocessing.Process(target=live_feedV2(), args=())
-    #maze = threading.Thread(target=live_feedV2(), args=())
-    #maze.start()
-    # live_feedV2()
-
-
-'''
-NOTES
-
-add detect holes in a loop so we make sure that all holes are detected
-
-
-
-
-'''
 
 
